6ccd873b6036c2a4da53f51d72538777eb3f60f9
merimak <jmtateossian50b@gmail.com>
2023-08-05 22:39:42 +0000

    removed debug

--- test.asm
+++ test.asm
@@ -1,10 +1,32 @@
 pushi 0
 pushi 1
 pushi 2
+shoot:
 etNew 0
-etCount 0 7 2
-etAim 0 1
-etAngle 0 0 0
-etSpeed 0 5 1
+etSprite 0 2 0
+etCount 0 4 4
+etAim 0 2
+etAngle 0 0 15
+etSpeed 0 9 1
 etOn 0
-wait 2000
+wait 60
+movePos 500 300
+etNew 1
+etSprite 1 4 0
+etCount 1 4 4
+etAim 1 1
+etAngle 1 0 15
+etSpeed 1 9 1
+etOn 1
+wait 60
+movePos 1000 300
+etNew 2
+etSprite 2 4 0
+etCount 2 4 4
+etAim 2 1
+etAngle 2 0 15
+etSpeed 2 9 1
+etOn 2
+wait 256
+movePos 0 0 
+jmp shoot
